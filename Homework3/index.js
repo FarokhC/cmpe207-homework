569129e55c8874c7ad5e6b8e0427d70a02488c49
Anjana Menon <anjana.menoncherubala@sjsu.edu>
2020-04-15 12:28:37 -0700

    Removed extra code

--- Homework3/index.js
+++ Homework3/index.js
@@ -6,35 +6,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-const getOnlineUsers = () => {
-  let clients = io.sockets.clients().connected;
-  let sockets = Object.values(clients);
-  let users = sockets.map(s => s.user);
-  return users.filter(u => u != undefined);
-};
-
 io.on('connection', function(socket){
 
- const emitOnlineUsers = () => {
-      socket.broadcast.emit("users", getOnlineUsers());
-    };
-  socket.on("add_user", user => {
-         socket.emit("server_message", {
-           name: website,
-           message: `Welcome to the Chat App !`
-         });
-
-         socket.broadcast.emit("server_message", {
-           name: website,
-           message: `${user.name} just Joined Chat`
-         });
-
-         socket.user = user;
-         emitOnlineUsers();
-       });
   socket.on('chat message', function(msg){
     socket.broadcast.emit('chat message', msg);
   });
+
   socket.on('typing', function(msg){
     console.log("typing");
     if(msg.nickname) {
@@ -46,18 +23,6 @@ io.on('connection', function(socket){
   });
   socket.broadcast.emit('hi');
 
-  socket.on("disconnect", function() {
-          const { user } = socket;
-
-          if (user) {
-            socket.broadcast.emit("server_message", {
-              name: website,
-              message: `${user.name} just left chat`
-            });
-          }
-
-          emitOnlineUsers();
-        });
 });
 
 http.listen(3000, function(){
