1d02da6ed113b88bb4f040a5b39b2242b4211e61
Farokh <lyghtsped@gmail.com>
2020-04-17 23:19:20 -0700

    Added connect/disconnect functionality

--- Homework3/index.js
+++ Homework3/index.js
@@ -6,8 +6,43 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var connectedUsers = [];
+
 io.on('connection', function(socket){
+//   io.emit('chat message', 'someone connected');
+
+  socket.on('disconnect', function(msg) {
+    try {
+      nickname = io.sockets.manager.roomClients[socket.id];
+      socket.broadcast.emit('disconnected', {disconnected_nickname: nickname});
+    }
+    catch {
+      console.log("couldn't find socket nickname");
+    }
+  });
+
+//   //handling disconnects
+  socket.on('disconnect_manually', function(msg) {
+    console.log("disconnecting " + msg.nickname);
+    console.log("connected users: " + JSON.stringify(connectedUsers));
+    console.log("msg name: " + msg.nickname)
+    user_index = connectedUsers.indexOf(msg.nickname);
+    connectedUsers.splice(user_index, 1);
+    console.log("connected users: " + JSON.stringify(connectedUsers));
+    socket.emit('disconnected', {connected: false});
+    socket.broadcast.emit('disconnected', {nickname: msg.nickname});
+  });
+
+  socket.on('sign_in', function(msg){
+    console.log("signing in " + msg.nickname);
+    connectedUsers.push(msg.nickname);
+    socket.join(msg.nickname);
+    socket.emit('sign_in', {connected: true});
+    socket.broadcast.emit('connected', {nickname: msg.nickname});
+  });
+
   socket.on('chat message', function(msg){
+    console.log("got chat message")
     socket.broadcast.emit('chat message', msg);
   });
   socket.on('typing', function(msg){
@@ -19,7 +54,6 @@ io.on('connection', function(socket){
       socket.broadcast.emit('typing', null);
     }
   });
-  socket.broadcast.emit('hi');
 });
 
 http.listen(3000, function(){
